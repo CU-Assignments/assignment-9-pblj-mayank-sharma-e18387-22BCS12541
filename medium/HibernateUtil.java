c6813108953a26fd13cabb8910a4383762662d22
Teesha <22bcs12541@cuchd.in>
2025-04-22 15:19:46 +0530

    Create HibernateUtil.java

---
+++ medium/HibernateUtil.java
@@ -0,0 +1,23 @@
+package medium;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError("Initial SessionFactory creation failed: " + ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
